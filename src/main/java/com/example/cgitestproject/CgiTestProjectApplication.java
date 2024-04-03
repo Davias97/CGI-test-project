d228f2c70199e6847228fa8aabec95e1cd86e2f0
Davias97 <da.vesely@email.cz>
2024-04-03 16:31:55 +0200

    project created

---
+++ src/main/java/com/example/cgitestproject/CgiTestProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.cgitestproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CgiTestProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CgiTestProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/cgitestproject/CgiTestProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.cgitestproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CgiTestProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
