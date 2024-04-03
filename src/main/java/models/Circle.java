81fe79524e484df5f356c44743c8236704ca7a6c
Davias97 <da.vesely@email.cz>
2024-04-03 16:54:55 +0200

    entities point and circle added

---
+++ src/main/java/models/Circle.java
@@ -0,0 +1,16 @@
+package models;
+
+public class Circle {
+
+    private Point circleCentre;
+    private double radius;
+
+    public Circle (Point centre, double radius) {
+        this.circleCentre = centre;
+        this.radius = radius;
+    }
+
+    public boolean isInside(Point point) {
+        
+    }
+}
